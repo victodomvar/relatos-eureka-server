4660aa23fffca040bd634e803bd9c96d0367e174
victodomvar <victor.dominguez506@comunidadunir.net>
2026-05-29 08:50:55 +0200

    Initialize Eureka server project with basic configuration and structure

---
+++ src/main/java/com/relatosdepapel/eureka/RelatosEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.relatosdepapel.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RelatosEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelatosEurekaServerApplication.class, args);
+    }
+}
